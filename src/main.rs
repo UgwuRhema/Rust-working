f043134bdd628a8ae277ab05deb6b0ea9adb04c4
UgwuRhema <rhemauros@protonmail.com>
2026-09-08 20:50:46 +0000

    Ahah i've won hehe

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 fn add(mut a: u64, mut b: u64) -> u64 {
-
+    a + b
 }
 
 fn main(){
-
+    let a: u64 = 56;
+    let b: u64 = 50;
+    let result: u64 = add(a,b);
+    println!("The resukt is {}", result);
 }
