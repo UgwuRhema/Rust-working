7a0034a77718b90ff6f1fe005600e265b8fba12f
UgwuRhema <rhemauros@protonmail.com>
2026-09-09 17:55:01 +0000

    Expanding

---
+++ src/calculator.rs
@@ -0,0 +1,6 @@
+use std::env::{args, Args}; //for command line arguments
+
+fn main()
+{
+	println!("Welcome to the CLI Calculator!");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+struct Human {
+	name: String,
+}
+
 fn add(a: u64, b: u64) -> u64 {
     a + b
 }
