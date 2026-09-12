a0899f74dd8a20c75fa03f43c8e7da9d481583ba
UgwuRhema <rhemauros@protonmail.com>
2026-09-12 19:34:57 +0000

    Okay...

--- src/student_mgmt.rs
+++ src/student_mgmt.rs
@@ -28,16 +28,10 @@ impl Student
 
 fn main()
 {
-    //just checking out somethings first...
+    //student vector...
     let mut students: Vec<Student> = Vec::new();
-    let student1: Student = Student::new("Ugwu Rhema", 17, 1);
-    let student2: Student = Student::new("Phat Nickher", 16, 2);
-    let student3: Student = Student::new("Balls Kennedy", 17, 3);
-    students.push(student1);
-    students.push(student2);
-    students.push(student3);
-
-    for i in 0..3 {
-        students[i].print_stud();
-    }
+    print!("Enter Student name: ");
+    let mut name: = String = String::new();
+    io::stdout().flush().expect("Failed to flush");
+    io::stdin().read_line(&mut name).expect("Failed to read into name");
 }
