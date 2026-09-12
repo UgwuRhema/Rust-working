57673cd5bfb839d7bb10885e0ac248c526b0e260
UgwuRhema <rhemauros@protonmail.com>
2026-09-12 19:13:21 +0000

    Are those how vectors work again?

--- src/student_mgmt.rs
+++ src/student_mgmt.rs
@@ -11,6 +11,22 @@ impl Student
 {
     fn new(n: &str, a: u32, i: u32) -> Self
     {
-
+        Self {
+            name: n.to_string(),
+            age: a,
+            id: i
+        }
     }
+
+    fn print_stud(&self)
+    {
+        println!("Name: {}", self.name);
+        println!("Age: {}", self.age);
+        println!("Id: {}", self.id);
+    }
+}
+
+fn main()
+{
+    let Students: Vec<Student> = Vec::new();
 }
