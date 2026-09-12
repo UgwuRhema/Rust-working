a1012a9976e6b38f5527acf487a21226310cbaf1
UgwuRhema <rhemauros@protonmail.com>
2026-09-12 19:23:25 +0000

    Works as i'd expect

--- src/student_mgmt.rs
+++ src/student_mgmt.rs
@@ -29,11 +29,15 @@ impl Student
 fn main()
 {
     //just checking out somethings first...
-    let mut Students: Vec<Student> = Vec::new();
+    let mut students: Vec<Student> = Vec::new();
     let student1: Student = Student::new("Ugwu Rhema", 17, 1);
     let student2: Student = Student::new("Phat Nickher", 16, 2);
     let student3: Student = Student::new("Balls Kennedy", 17, 3);
-    Students.push(student1);
-    Students.push(student2);
-    Students.push(student3);
+    students.push(student1);
+    students.push(student2);
+    students.push(student3);
+
+    for i in 0..3 {
+        students[i].print_stud();
+    }
 }
