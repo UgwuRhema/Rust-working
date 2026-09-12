e0cbc5db0439aa896b534a40d1ba011ffc8530b8
UgwuRhema <rhemauros@protonmail.com>
2026-09-12 19:09:27 +0000

    Getting the hang of this

---
+++ src/student_mgmt.rs
@@ -0,0 +1,16 @@
+use std::io::{self, Write};
+
+struct Student
+{
+    name: String,
+    age: u32,
+    id: u32,
+}
+
+impl Student
+{
+    fn new(n: &str, a: u32, i: u32) -> Self
+    {
+
+    }
+}
